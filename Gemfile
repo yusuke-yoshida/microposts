253d631e5a97b4ae4cad6fe2ff231dd9016cbb77
yusuke_yoshida <yoshidy37@gmail.com>
2015-09-20 14:04:11 +0000

    added pagination

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'bootstrap-sass'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+# Use Pagination "Kaminari"
+gem 'kaminari'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build if logged_in?
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
 end
